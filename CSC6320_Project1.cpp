02c1294c71863d2879efa3a04c01b180ea7679fb
yosammy01 <samueleverardorobles@gmail.com>
2025-06-28 14:44:25 -0400

    Step 1: Read Process Data from a File
    Completed task and tested negative cases.

--- CSC6320_Project1.cpp
+++ CSC6320_Project1.cpp
@@ -1,20 +1,102 @@
 // CSC6320_Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-int main()
+using namespace std;
+
+int Check_Format(int argc, char* argv[])
 {
-    std::cout << "Hello Yall!\n";
+    // do check for proper argument format
+    if (argc != 3)
+    {
+        cout << "WRONG INPUT FORMAT! \n" << endl;
+        cout << "Proper input format:" << endl;
+        cout << "[executable name] [path to file] [filename]\n" << endl;
+        cout << "Example:" << endl;
+        cout << "CSC6320_project1 " << "C:\\temp " << "process.txt" << endl;
+        return 1;
+    }
+    cout << "Argument count is correct." << endl;
+    cout << "Arguments = " << argc << endl;
+
+    // loop through arguments
+    int i = 0;
+    for (int i = 0; i < argc; i++)
+    {
+        cout << "Argument " << i << " = " << argv[i] << endl;
+    }
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+int main(int argc, char* argv[])
+{
+    // check for argument format errors
+    if (Check_Format(argc, argv))
+    {
+        return 0;
+    }
+
+    // read data file and assume proper data format.
+    string filePath = argv[1];
+    filePath.append("\\");
+    filePath.append(argv[2]);
+    cout << "File path = " + filePath << endl;
+    ifstream dataFile(filePath);
+
+    if (!dataFile.is_open())
+    {
+        cout << "ERROR OPENING FILE!" << endl;
+        return 0;
+    }
+    cout << "Data file contents:" << endl;
+
+    //read data line-by-line and store in a vector of integer vectors that will store the data.
+    vector<int> dataVector = {0,0,0,0};
+    vector<vector<int>> dataVectorList;
+    string line;
+    string word;
+
+    int lineNumber = 0;
+    while (getline(dataFile, line))
+    {
+        cout << line << endl;
+
+        // Skip the first line containing the column names
+        if (lineNumber == 0)
+        {
+            lineNumber++;
+            continue;
+        }
+
+        // Extract words one by one until the end of each line and store in data vector
+        istringstream iss(line);
+        int wordCount = 0;
+        while (iss >> word)
+        {
+            dataVector[wordCount] = stoi(word);
+            wordCount++;
+        }
+
+        // store each data vector in the list of vectors
+        dataVectorList.push_back(dataVector);
+    }
+
+    // display the data stored in dataVectorList memory.
+    for (int i=0; i<(int)dataVectorList.size(); i++)
+    {
+        cout << "dataVectorList[" << i << "] = ";
+        for (int j=0; j<(int)dataVector.size(); j++)
+        {
+            cout << dataVectorList[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
